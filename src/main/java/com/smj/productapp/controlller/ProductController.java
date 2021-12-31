da7cbb96b133a153891f3272e9fd542e2a0199d3
hp <suvarin.kapase@gmail.com>
2021-12-31 14:12:18 +0530

    Product Controller updated

--- src/main/java/com/smj/productapp/controlller/ProductController.java
+++ src/main/java/com/smj/productapp/controlller/ProductController.java
@@ -1,10 +1,41 @@
 package com.smj.productapp.controlller;
 
+import com.smj.productapp.model.Products;
 import com.smj.productapp.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+@RestController
+@RequestMapping("/Products")
 public class ProductController {
 
     @Autowired
     ProductService service;
+
+    @PostMapping
+    public Mono<Products> saveProduct(@RequestBody Products product){
+      return service.saveProduct(product);
+    }
+
+    @GetMapping("/{id}")
+    public Mono<Products> findById(@PathVariable("id") Long id){
+        return service.findById(id);
+    }
+
+    @GetMapping
+    public Flux<Products> getAllProducts(){
+        return service.getAllProducts();
+    }
+
+    @PutMapping("/{id}")
+    public Mono<Products> updateProduct(@RequestBody Products product){
+        return service.updateProduct(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<Void> deleteProduct(Long id){
+        return service.deleteProduct(id);
+    }
 }
