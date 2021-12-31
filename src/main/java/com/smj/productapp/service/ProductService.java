25c04bc05bee029ec2f0c44141cc42a56c49986f
hp <suvarin.kapase@gmail.com>
2021-12-31 13:55:40 +0530

    Skelton of project is created

--- src/main/java/com/smj/productapp/service/ProductService.java
+++ src/main/java/com/smj/productapp/service/ProductService.java
@@ -1,5 +1,18 @@
 package com.smj.productapp.service;
 
+import com.smj.productapp.model.Products;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public interface ProductService {
 
+    Mono<Products> saveProduct(Products product);
+
+    Mono<Products> findById(Long id);
+
+    Flux<Products> getAllProducts();
+
+    Mono<Products> updateProduct(Products product);
+
+    Mono<Void> deleteProduct(Long id);
 }
--- src/main/java/com/smj/productapp/service/ProductServiceImpl.java
+++ src/main/java/com/smj/productapp/service/ProductServiceImpl.java
@@ -1,12 +1,40 @@
 package com.smj.productapp.service;
 
 import com.smj.productapp.dao.ProductDao;
+import com.smj.productapp.model.Products;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class ProductServiceImpl implements ProductService{
 
     @Autowired
     ProductDao dao;
+
+    @Override
+    public Mono<Products> saveProduct(Products product) {
+        return dao.save(product);
+    }
+
+    @Override
+    public Mono<Products> findById(Long id) {
+        return dao.findById(id);
+    }
+
+    @Override
+    public Flux<Products> getAllProducts() {
+        return dao.findAll();
+    }
+
+    @Override
+    public Mono<Products> updateProduct(Products product) {
+        return dao.save(product);
+    }
+
+    @Override
+    public Mono<Void> deleteProduct(Long id) {
+        return dao.deleteById(id);
+    }
 }
